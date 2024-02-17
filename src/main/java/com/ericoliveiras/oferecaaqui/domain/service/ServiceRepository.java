7beb78cd8e7ee8e9a06915530c2caf30bbd9aa69
Eric Oliveira <oiericoi@hotmail.com>
2024-02-17 13:45:18 -0300

    feat: add service repository

---
+++ src/main/java/com/ericoliveiras/oferecaaqui/domain/service/ServiceRepository.java
@@ -0,0 +1,6 @@
+package com.ericoliveiras.oferecaaqui.domain.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
