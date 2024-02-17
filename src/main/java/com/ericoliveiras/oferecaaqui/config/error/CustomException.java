5ac69de9cda5dd832ed6ee17b17984127e0d4b6a
Eric Oliveira <oiericoi@hotmail.com>
2024-02-17 11:55:52 -0300

    feat: add custom exception

---
+++ src/main/java/com/ericoliveiras/oferecaaqui/config/error/CustomException.java
@@ -0,0 +1,16 @@
+package com.ericoliveiras.oferecaaqui.config.error;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+  public HttpStatus status;
+
+  public CustomException(String message, HttpStatus status) {
+    super(message);
+    this.status = status;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+}
